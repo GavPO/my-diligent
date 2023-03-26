4c4925e8d50085c0aa17c2c210fc315b1966f008
Gavin Orlich <gavinporlich@gmail.com>
2023-03-26 13:57:57 -0500

    serve build

--- server/index.js
+++ server/index.js
@@ -21,6 +21,10 @@ mongoose
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.log(err.message));
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
+
 const server = app.listen(process.env.PORT || 5000, () =>
   console.log(`Server is running on port: ${process.env.PORT}`)
 );
